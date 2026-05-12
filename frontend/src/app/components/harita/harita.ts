b19e1d4d4af0a17fb197d69b7053385cfaad46e6
oioxo <ozkanarda.012@gmail.com>
2026-05-13 02:11:15 +0400

    =

--- frontend/src/app/components/harita/harita.ts
+++ frontend/src/app/components/harita/harita.ts
@@ -13,26 +13,27 @@ export class HaritaComponent implements OnInit {
   depoListesi: any[] = [];
   seciliDepo: any = null;
 
-  // image_10.png (1000px genişlik) referans alınarak kalibre edilmiş koordinatlar
-  sehirKoordinatlari: any = {
-    'İstanbul': { x: 250, y: 140 }, // Marmara'nın kuzeybatısı
-    'Ankara':   { x: 510, y: 230 }, // Tam merkez İç Anadolu
-    'İzmir':    { x: 130, y: 310 }, // Ege kıyısı
-    'Bursa':    { x: 260, y: 190 }, // Marmara'nın güneyi
-    'Antalya':  { x: 380, y: 410 }, // Güney kıyısı
-    'Adana':    { x: 600, y: 395 }, // Akdeniz'in doğusu
-    'Erzurum':  { x: 860, y: 220 }, // Doğu Anadolu
-    'Samsun':   { x: 620, y: 130 }  // Orta Karadeniz kıyısı
-  };
+  // Harita üzerindeki X (yatay) ve Y (dikey) konumları. 
+  // (0-1000 arası X, 0-450 arası Y olacak şekilde kalibre edildi)
+  sehirRehberi: any = {
+  'İstanbul': { x: 200, y: 125 }, // Marmara'nın tam yeri
+  'Ankara':   { x: 440, y: 220 }, // İç Anadolu'nun ortası
+  'İzmir':    { x: 105, y: 295 }, // Ege kıyısı
+  'Bursa':    { x: 225, y: 170 }, 
+  'Antalya':  { x: 340, y: 395 }, // Güney kıyısı
+  'Adana':    { x: 560, y: 390 },
+  'Erzurum':  { x: 830, y: 225 }, // Doğu Anadolu
+  'Samsun':   { x: 580, y: 115 }  // Karadeniz kıyısı
+};
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    // Servisten gelen depoları dinle ve koordinatlarını bas
     this.dataService.currentDepolar.subscribe(res => {
       this.depoListesi = res.map(depo => ({
         ...depo,
-        // image_10.png üzerindeki koordinatları servisten gelen veriye göre atıyoruz
-        coords: this.sehirKoordinatlari[depo.sehir] || { x: 510, y: 230 } // Şehir yoksa Ankara'ya koy
+        coords: this.sehirRehberi[depo.sehir] || { x: 440, y: 220 } // Şehir yoksa Ankara'ya ata
       }));
     });
   }
